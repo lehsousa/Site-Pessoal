d9983b20aa532f736901fe701cbfd08f8cb49ea8
Leandro Sousa <leandro.sousa@gmail.com>
2021-10-11 07:48:58 -0300

    Site Pessoal

---
+++ main.js
@@ -0,0 +1,32 @@
+//Revel
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `
+.logo h1, .header-itens nav, .header-itens h1, .call , .about h1, .about-content img, .about-txt,
+.skills, .container, .frase, .social, .social p
+
+`, {
+        interval: 150
+    }
+)
+
+//back to top
+window.addEventListener('scroll', function() {
+    const btnHome = document.querySelector('.btn-home')
+    btnHome.classList.toggle('sticky', window.scrollY > 0)
+
+    if (window.scrollY > 0) {
+        const arrowOn = document.querySelector('#arrowUp')
+        arrowOn.style.display = 'initial'
+    } else {
+        const arrowOff = document.querySelector('#arrowUp')
+        arrowOff.style.display = 'none'
+    }
+})
